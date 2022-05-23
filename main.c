5ddafdf6afbf92761dfb49addc02db42ed9008bb
Lucas <Lucaspereiraferreira055@gmail.com>
2022-05-23 16:57:21 -0300

    continuacao da logica

--- main.c
+++ main.c
@@ -3,12 +3,11 @@
 #include <stdbool.h>
 
 struct Parede{
-		
 	float altura;
 	float largura;
-	int portas;
 	int qtdjanelas;
-	
+	int qtdportas;
+	int totalTintas;
 }parede;
 	
 struct Comodo{
@@ -21,9 +20,20 @@ void clearr(){
 	system("@cls || clear");
 }
 
+
+bool calculaAlturaMinima(float alturaParede, int qtdPortas) {
+	printf("altura parede: %.2f\n", alturaParede);
+	printf("qtdPortas: %d\n", qtdPortas);
+	
+	
+	if ((qtdPortas >= 0 && ((alturaParede - 0.3) >= 1.9)) || qtdPortas == 0) {
+		return true;
+	}
+	else return false;
+}
+	
 bool calculaAreaMinima(float altura, float largura){
 	float area = altura * largura;
-	
 	
 	if(area >= 1 && area <= 15){
 		return true;
@@ -32,9 +42,26 @@ bool calculaAreaMinima(float altura, float largura){
 		return false;
 		
 }
-bool areaPortasJanelas(struct Parede parede){
-	float area = portas* janela
+bool proporcaoArea(struct Parede parede){
+	float areaParede = parede.altura * parede.largura;
+	float areaPortas = 1.52 * parede.qtdportas;
+	float areaJanelas = 2.40 * parede.qtdjanelas;
+	
+	 if ((areaPortas + areaJanelas) > (areaParede/2)){
+	 	return false;
+	 }
+	else
+		return true;
 }
+
+void totalTintas(struct Comodo comodo){
+	while(totalTintas >=1 * comodo.paredes.altura * comodo.paredes.largura <= 20 ){
+		comodo.paredes[0].altura * comodo.paredes[0].largura
+	}
+}
+
+
+
 int main (){
 	int i =0;
 	struct Comodo comodo;
@@ -48,14 +75,26 @@ int main (){
 		scanf("%f",&comodo.paredes[i].largura);
 		
 		printf("Qtd. portas (0 se nao houver): ");
-		scanf("%d",&comodo.paredes[i].portas);
+		scanf("%d",&comodo.paredes[i].qtdportas);
 		
 		printf("Qtd. janela (0 se nao houver): ");
 		scanf("%d",&comodo.paredes[i].qtdjanelas);
 		
 		if (calculaAreaMinima(comodo.paredes[i].altura, comodo.paredes[i].largura)) {
+			if (proporcaoArea(comodo.paredes[i]) == true)
+				if(calculaAlturaMinima(comodo.paredes[i].altura, comodo.paredes[i].qtdportas) == true)
+					i++;
+				else {
+//					clearr();
+					
+					printf("erro: altura da parede deve ser pelo menos 30cm da altura da portas!!");
+				}
+			else {
+				clearr();
+				
+				printf("erro: a soma da area das portas e janelas nao pode ultrapassar 50%% da area da parede!!");
+			}
 			
-			i++;
 		}
 		else {
 			clearr();
